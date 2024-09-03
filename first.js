a0550da88fc3db3a4537542a194de83ba49c34ea
Shakir Waheed <shakir03748@gmail.com>
2024-09-03 22:16:57 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+console.log("hello 🤣🤣🤣😊😊");
+console.log(2+5)
+a=5;
+console.log(typeof(a))
+<i class="fa-solid fa-database"></i>
+<i class="fa-solid fa-computer"></i>
+ <i class="fa-solid fa-code"></i>
